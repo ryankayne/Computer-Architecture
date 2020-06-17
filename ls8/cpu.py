ec8abc3baa208c8bd39a9d6d24bc64c8c1150ae3
ryankayne <coope1rk@gmail.com>
2020-06-16 23:06:46 -0400

    did lots of stuff. Day 1 done and some of day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,13 +6,39 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256        # 256 bytes of memory
+        self.register = [0] * 8     # 8 registers
+        self.pc = 0                 # program counter
+        # self.halted = False         # if halted or not (T/F)
+        self.sp = 7                 # stack pointer
+        self.running = True
 
-    def load(self):
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def load(self, file):
         """Load a program into memory."""
 
-        address = 0
+        # address = 0
+
+        # with open(file,'r') as f:
+        #     all_lines = f.readlines()
+
+        with open(file) as f:
+            address = 0
+
+            for line in f:
+                line_split = line.split("#")
+                num = line_split[0].strip()
+                if num == "":
+                    continue
+                val = int(num, 2)
+                self.ram[address] = val
+
+                address += 1
 
         # For now, we've just hardcoded a program:
 
@@ -26,9 +52,9 @@ class CPU:
             0b00000001, # HLT
         ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -62,4 +88,39 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        # Program counter, the index (address) of the current instruction
+        # Reads the memory address that's stored in register
+
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        while self.running:
+        # Stores the result in "Instruction Register" from the memory (RAM) address in PC
+            ir = self.ram[self.pc]
+            # `LDI` instruction (EX: SAVE_REG in comp.py)
+            if ir == LDI:
+                address = self.ram[self.pc + 1]
+                value = self.ram[self.pc + 2]
+                self.register[address] = value
+                self.pc += 3
+            # `PRN` instruction (EX: PRINT_REG in comp.py)
+            elif ir == PRN:
+                address = self.ram[self.pc + 1]
+                print(self.register[address])
+                self.pc += 2
+            # `HLT` instruction (EX: HALT in comp.py)
+            elif ir == HLT:
+                self.running = False
+                self.pc += 1
+        # ELSE STATEMENT from comp.py
+            else:
+                print(f'Unknown instruction {ir} at address {self.pc}')
+                break
+        # self.trace()
+
+# cpu = CPU()
+
+# cpu.load()
+# cpu.load()
+# cpu.run()
